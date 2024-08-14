6336c60317c09e56ccdad7b08a974291251c8e97
Nicolec123 <danielefloripa42@gmail.com>
2024-08-14 13:58:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,43 +2,55 @@
 let messagesHistory = []
 
 // Envio do formulário de login
-document
-  .getElementById("loginForm")
-  .addEventListener("submit", async (event) => {
-    event.preventDefault()
-    const username = document.getElementById("username").value
-    const password = document.getElementById("password").value
-    console.log("Tentando fazer login com o usuário:", username)
-    try {
-      const response = await fetch(
-        "https://hmlg.portalbeneficiocerto.com.br/chat_certo/sessions",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ username, password }),
-        }
-      )
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
+document.getElementById("loginForm").addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  console.log("Tentando fazer login com o usuário:", username);
+  
+  try {
+    const response = await fetch(
+      "https://hmlg.portalbeneficiocerto.com.br/chat_certo/sessions",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
       }
-      const data = await response.json()
-      console.log("Login response:", data)
-      localStorage.setItem("access_token", data.access_token)
-      document.getElementById("loginContainer").style.display = "none"
-      document.getElementById("home-container").style.display = "block"
-      
-      // Esse é o carrossel que aparece automaticamente ao fazer o login e entrar na tela de chat
-      document.getElementById("carousel").style.display = "flex"  /* isso é responsável por ele aparecer*/ 
-      
-      // Atualiza o histórico com as mensagens armazenadas
-      updateMessagesDiv()
-    } catch (error) {
-      console.error("Failed to login:", error.message)
-      alert("Falha ao fazer login, tente novamente!")
+    );
+
+    if (!response.ok) {
+      if (response.status === 500) {
+        throw new Error("Chat indisponível no momento, por favor tente mais tarde!");
+      } else {
+        throw new Error("Falha ao enviar mensagem, verifique se o usuário e senha estão certos e tente novamente!");
+      }
     }
-  })
+
+    const data = await response.json();
+    console.log("Login response:", data);
+    localStorage.setItem("access_token", data.access_token);
+
+    // Ocultar a tela de login e a imagem de fundo, e exibir o chat
+    document.getElementById("loginContainer").style.display = "none";
+    const homeContainer = document.getElementById("home-container");
+    homeContainer.style.display = "block";
+
+    // Ocultar a imagem de fundo
+    document.querySelector(".imagem-de-fundo").classList.add("hidden");
+    
+    // Exibir o carrossel automaticamente ao entrar no chat
+    document.getElementById("carousel").style.display = "flex"; 
+    
+    // Atualiza o histórico com as mensagens armazenadas
+    updateMessagesDiv();
+  } catch (error) {
+    console.error("Failed to login:", error.message);
+    alert(error.message);
+  }
+});
+ 
 
 // Envio do formulário de chat
 document
@@ -48,8 +60,10 @@ document
     const message = document.getElementById("message").value
     const token = localStorage.getItem("access_token")
     console.log("Enviando mensagem:", message)
+    
     // Mostrar o GIF de carregamento
     document.getElementById("loadingGif").style.display = "block"
+    
     try {
       const response = await fetch(
         "https://hmlg.portalbeneficiocerto.com.br/chat_certo/ai/chat_certo",
@@ -62,37 +76,51 @@ document
           body: JSON.stringify({ message }),
         }
       )
+      
       if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`)
+        if (response.status === 500) {
+          throw new Error("Chat indisponível no momento, por favor tente mais tarde!")
+        } else {
+          throw new Error("Falha ao enviar mensagem, verifique o conteúdo digitado e tente novamente!")
+        }
       }
+      
       const data = await response.json()
       console.log("Chat response:", data)
       const messagesDiv = document.getElementById("messages")
+      
       // Adicionar a mensagem do usuário
       const userMessage = {
         type: "user",
         content: message,
       }
+      
       // Adicionar a mensagem do bot
       const botMessageContent = data.result
         ? data.result
-        : "Infelizmente, sou incapaz de responder a esta pergunta. Caso precise de mais assistência, estarei à sua disposição!:)"
+        : "Infelizmente, sou incapaz de responder a esta pergunta. Caso precise de mais assistência, estarei à sua disposição! :)"
+        
       // Converter resposta para tabela
       const botMessageTable = convertResponseToTable(botMessageContent)
       const botMessage = {
         type: "bot",
         content: botMessageTable,
       }
+      
       // Adicionar as mensagens ao histórico
       messagesHistory.push(userMessage)
       messagesHistory.push(botMessage)
+      
       // Atualizar a visualização de mensagens
       updateMessagesDiv()
+      
       // Limpar o campo de entrada de mensagem
       document.getElementById("message").value = ""
+      
     } catch (error) {
       console.error("Failed to send message:", error.message)
-      alert("Falha ao enviar mensagem, tente novamente!")
+      alert(error.message)
+      
     } finally {
       // Ocultar o GIF de carregamento
       document.getElementById("loadingGif").style.display = "none"
@@ -153,29 +181,35 @@ document.getElementById("deleteAllButton").addEventListener("click", () => {
   deleteAllMessages()
 })
 
-//  botão de logout
+
+// Botão de logout
 document.getElementById("logoutButton").addEventListener("click", () => {
-  // Remove o token de acesso do localStorage
-  localStorage.removeItem("access_token")
-
-  // Remove o histórico de mensagens do localStorage
-  localStorage.removeItem("messagesHistory")
-
-  // Limpa o histórico de mensagens
-  messagesHistory = []
-  const messagesContainer = document.getElementById("messages")
-  messagesContainer.innerHTML = ""
-
-  // Limpa os campos de login
-  document.getElementById("username").value = ""
-  document.getElementById("password").value = ""
-
-  // Limpa o campo de entrada de mensagem atual
-  document.getElementById("message").value = ""
-
-  // Exibe o container de login e oculta o container de chat
-  document.getElementById("loginContainer").style.display = "block"
-  document.getElementById("home-container").style.display = "none"
+  // Alerta de confirmação para o logout
+  const userConfirmed = confirm("Você tem certeza de que deseja sair? Todas as suas mensagens serão apagadas.")
+  
+  if (userConfirmed) {
+    // Remove o token de acesso do localStorage
+    localStorage.removeItem("access_token")
+  
+    // Remove o histórico de mensagens do localStorage
+    localStorage.removeItem("messagesHistory")
+  
+    // Limpa o histórico de mensagens
+    messagesHistory = []
+    const messagesContainer = document.getElementById("messages")
+    messagesContainer.innerHTML = ""
+  
+    // Limpa os campos de login
+    document.getElementById("username").value = ""
+    document.getElementById("password").value = ""
+  
+    // Limpa o campo de entrada de mensagem atual
+    document.getElementById("message").value = ""
+  
+    // Exibe o container de login e oculta o container de chat
+    document.getElementById("loginContainer").style.display = "block"
+    document.getElementById("home-container").style.display = "none"
+  }
 })
 
 // Exibir o carrossel com as imagens de exemplo
